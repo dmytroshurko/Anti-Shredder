8107a31d1ca631a763a4283dc13da8aa51f64233
dmytroshurko <dmysh@protonmail.com>
2020-12-24 17:25:49 +0200

    add function for image rotation

--- include/imutils.hpp
+++ include/imutils.hpp
@@ -4,7 +4,7 @@
 #include <opencv2/core.hpp>
 
 /*
- * @brief Resizes an image using a computed ratio between new and old width (or
+ * @brief Resizes the image using a computed ratio between new and old width (or
  * height).
  *
  * @param src The input image.
@@ -12,7 +12,17 @@
  * @param width The desired width of the output image.
  * @param height The desired height of the output image.
  */
-void resize(const cv::Mat& src, cv::Mat& dst, int width = 0,
+void Resize(const cv::Mat& src, cv::Mat& dst, int width = 0,
             int height = 0) noexcept;
+
+/*
+ * @brief Rotates the image without cropping.
+ *
+ * @param src The input image.
+ * @param dst The output image.
+ * @param angle The image rotation angle (in degrees).
+ */
+void RotateWithoutCropping(const cv::Mat& src, cv::Mat& dst,
+                           double angle) noexcept;
 
 #endif  // IMUTILS_HPP_
--- src/imutils.cpp
+++ src/imutils.cpp
@@ -2,7 +2,7 @@
 
 #include <opencv2/imgproc.hpp>
 
-void resize(const cv::Mat& src, cv::Mat& dst, int width, int height) noexcept {
+void Resize(const cv::Mat& src, cv::Mat& dst, int width, int height) noexcept {
   int w = src.cols;
   int h = src.rows;
 
@@ -23,4 +23,26 @@ void resize(const cv::Mat& src, cv::Mat& dst, int width, int height) noexcept {
   }
 
   cv::resize(src, dst, dsize);
+}
+
+void RotateWithoutCropping(const cv::Mat& src, cv::Mat& dst,
+                           double angle) noexcept {
+  int w = src.cols;
+  int h = src.rows;
+  int cx = w / 2;
+  int cy = h / 2;
+
+  cv::Mat M = cv::getRotationMatrix2D(cv::Point2f(cx, cy), -angle, 1.0);
+  double cos = cv::abs(M.at<double>(0, 0));
+  double sin = cv::abs(M.at<double>(0, 1));
+
+  // compute the new width and height of the image
+  int nw = static_cast<int>((h * sin) + (w * cos));
+  int nh = static_cast<int>((h * cos) + (w * sin));
+
+  // adjust the rotation matrix to take into account translation
+  M.at<double>(0, 2) += (nw / 2.0) - cx;
+  M.at<double>(1, 2) += (nh / 2.0) - cy;
+
+  cv::warpAffine(src, dst, M, cv::Size(nw, nh));
 }
