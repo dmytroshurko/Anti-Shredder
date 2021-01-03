724d5e23a8765f6f5e7322cac74c045f28612324
dmytroshurko <dmysh@protonmail.com>
2021-01-03 18:18:36 +0200

    add a function that returns image strips from the input image

--- include/anti_shredder.hpp
+++ include/anti_shredder.hpp
@@ -8,8 +8,20 @@
  * @brief Detects rectangles around the paper strips in the image.
  *
  * @param src The input image.
- * @return The vector with detected rectangles.
+ * @return A vector with detected rectangles.
  */
-std::vector<cv::RotatedRect> detect_rectangles(const cv::Mat& src);
+std::vector<cv::RotatedRect> DetectRectangles(const cv::Mat& src);
+
+/*
+ * @brief Returns image strips from the input image.
+ *
+ * @param src The input image.
+ * @param rectangles A vector with detected rectangles.
+ * @param approx_iterations Number of iterations to approximate the rectangle.
+ * @return A vector with extracted image strips.
+ */
+std::vector<cv::Mat> CropRectangles(
+    const cv::Mat& src, const std::vector<cv::RotatedRect>& rectangles,
+    int approx_iterations = 1);
 
 #endif  // ANTI_SHREDDER_HPP_
--- src/anti_shredder.cpp
+++ src/anti_shredder.cpp
@@ -2,8 +2,11 @@
 
 #include <algorithm>
 #include <opencv2/imgproc.hpp>
+#include <utility>
 
-std::vector<cv::RotatedRect> detect_rectangles(const cv::Mat& src) {
+#include "imutils.hpp"
+
+std::vector<cv::RotatedRect> DetectRectangles(const cv::Mat& src) {
   cv::Mat src_gray;
   cv::cvtColor(src, src_gray, cv::COLOR_BGR2GRAY);
 
@@ -12,9 +15,9 @@ std::vector<cv::RotatedRect> detect_rectangles(const cv::Mat& src) {
   cv::Mat canny_output;
   cv::Canny(src_gray, canny_output, 50, 150);
 
-  cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(10, 10));
-  cv::dilate(canny_output, canny_output, element, cv::Point(-1, -1), 2);
-  cv::erode(canny_output, canny_output, element, cv::Point(-1, -1), 2);
+  cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(12, 12));
+  cv::morphologyEx(canny_output, canny_output, cv::MORPH_CLOSE, element,
+                   cv::Point(-1, -1), 2);
 
   std::vector<std::vector<cv::Point>> contours;
   cv::findContours(canny_output, contours, cv::RETR_EXTERNAL,
@@ -43,4 +46,65 @@ std::vector<cv::RotatedRect> detect_rectangles(const cv::Mat& src) {
       rectangles.end());
 
   return rectangles;
+}
+
+std::vector<cv::Mat> CropRectangles(
+    const cv::Mat& src, const std::vector<cv::RotatedRect>& rectangles,
+    int approx_iterations) {
+  std::vector<cv::Mat> rect_images;
+
+  for (const auto& rectangle : rectangles) {
+    cv::Mat mask = cv::Mat::zeros(src.size(), CV_8UC1);
+
+    cv::Point2f pts[4];
+    rectangle.points(pts);
+
+    cv::Point vertices[4];
+    for (int i = 0; i < 4; ++i) {
+      vertices[i] = pts[i];
+    }
+
+    // create a mask for the current rectangle
+    cv::fillConvexPoly(mask, vertices, 4, cv::Scalar(255, 255, 255));
+
+    cv::Mat selected_rect;
+    cv::bitwise_and(src, src, selected_rect, mask);
+
+    cv::Mat rect_img = selected_rect(rectangle.boundingRect());
+
+    RotateWithoutCropping(rect_img, rect_img, -rectangle.angle);
+
+    // make sure that the rectangle is in a vertical position
+    if (rect_img.rows < rect_img.cols) {
+      cv::rotate(rect_img, rect_img, cv::ROTATE_90_CLOCKWISE);
+    }
+
+    int rw = rectangle.size.width;
+    int rh = rectangle.size.height;
+    if (rh < rw) {
+      std::swap(rw, rh);
+    }
+
+    CropCenter(rect_img, rect_img, cv::Size(rw, rh));
+
+    for (int i = 0; i < approx_iterations; ++i) {
+      cv::Mat gray;
+      cv::cvtColor(rect_img, gray, cv::COLOR_BGR2GRAY);
+
+      cv::GaussianBlur(gray, gray, cv::Size(3, 3), 0);
+
+      cv::Mat canny_output;
+      cv::Canny(gray, canny_output, 100, 200);
+
+      // calculate the up-right bounding rectangle of a non-zero pixels of
+      // gray-scale image
+      cv::Rect roi = cv::boundingRect(canny_output);
+
+      rect_img = rect_img(roi);
+    }
+
+    rect_images.emplace_back(rect_img);
+  }
+
+  return rect_images;
 }
